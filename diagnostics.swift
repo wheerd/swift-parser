7a399ebfa329e9b31c6903321a43bb506658afba
Wheerd <admin@wheerd.de>
2016-06-01 22:39:12 +0100

    Forgot to check in new files...

---
+++ diagnostics.swift
@@ -0,0 +1,62 @@
+class Diagnose
+{
+  typealias Index = String.UnicodeScalarView.Index
+
+  enum DiagnoseType
+  {
+    case Error, Warning, Note
+  }
+
+  struct FixIt
+  {
+    let range: Range<Index>
+    let replacement: String
+  }
+
+  let source: Source
+  let range: Range<Index>
+  let message: String
+  let type: DiagnoseType
+  var related = [Diagnose]()
+  var fixIts = [FixIt]()
+
+  init(_ message: String, type: DiagnoseType, range: Range<Index>, source: Source)
+  {
+    self.message = message
+    self.type = type
+    self.range = range
+    self.source = source
+  }
+
+  func getLine() -> Int
+  {
+    return self.source.getLine(index: self.range.lowerBound)
+  }
+
+  func getColumn() -> Int
+  {
+    return self.source.getColumn(index: self.range.lowerBound)
+  }
+
+  func getContext() -> (Range<Index>, Int, Int)
+  {
+    return self.source.getContext(index: self.range.lowerBound)
+  }
+
+  func withFixIt(range: Range<Index>, replacement: String) -> Diagnose
+  {
+    self.fixIts.append(FixIt(range: range, replacement: replacement))
+    return self
+  }
+
+  func withInsertFix(at: Index, insert: String) -> Diagnose
+  {
+    return self.withFixIt(range: at..<at, replacement: insert)
+  }
+
+  func withNote(_ message: String, range: Range<Index>, source: Source? = nil) -> Diagnose
+  {
+    self.related.append(Diagnose(message, type: .Note, range: range, source: source ?? self.source))
+    return self
+  }
+}
---
+++ source.swift
@@ -0,0 +1,182 @@
+import Foundation
+
+class Source
+{
+  typealias Index = String.UnicodeScalarView.Index
+
+  let content: String
+  let identifier: String
+  let characters: String.UnicodeScalarView
+  let start: Index
+  let end: Index
+
+  init(_ content: String, identifier: String)
+  {
+    self.content = content
+    self.identifier = identifier
+    self.characters = content.unicodeScalars
+    self.start = content.unicodeScalars.startIndex
+    self.end = content.unicodeScalars.endIndex
+  }
+
+  convenience init?(path: String)
+  {
+    if let content = try? NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+    {
+      self.init(String(content), identifier: path)
+    }
+    else
+    {
+      return nil
+    }
+  }
+
+  func index(after index: Index, offset: Int = 1) -> Index?
+  {
+    var index = index
+    var offset = offset
+    while index != self.end && offset > 0
+    {
+      index = self.characters.index(after: index)
+      offset -= 1
+    }
+    return index != self.end ? index : nil
+  }
+
+  func character(after index: Index, offset: Int = 1) -> UnicodeScalar?
+  {
+    if let nextIndex = self.index(after: index, offset: offset)
+    {
+      if nextIndex != self.end
+      {
+        return self.characters[nextIndex]
+      }
+    }
+
+    return nil
+  }
+
+  func index(before index: Index, offset: Int = 1) -> Index?
+  {
+    if offset == 0
+    {
+      return index
+    }
+
+    var index = index
+    var offset = offset
+    while index != self.start && offset > 0
+    {
+      index = self.characters.index(before: index)
+      offset -= 1
+    }
+
+    return (index != self.start || offset == 0) ? index : nil
+  }
+
+  func character(before index: Index, offset: Int = 1) -> UnicodeScalar?
+  {
+    if let prevIndex = self.index(before: index, offset: offset)
+    {
+      return self.characters[prevIndex]
+    }
+
+    return nil
+  }
+
+  func getLine(index: Index) -> Int
+  {
+    var index = index
+    var line = 1
+    var lastWasLF = false
+    while index != self.start
+    {
+      index = self.characters.index(before: index)
+      if self.characters[index] == "\r" && !lastWasLF
+      {
+        line += 1
+      }
+      if self.characters[index] == "\n"
+      {
+        lastWasLF = true
+        line += 1
+      }
+      else
+      {
+        lastWasLF = false
+      }
+    }
+
+    return line
+  }
+
+  func getColumn(index: Index) -> Int
+  {
+    var index = index
+    var col = 1
+    while index != self.start
+    {
+      index = self.characters.index(before: index)
+      if self.characters[index] == "\r" || self.characters[index] == "\n"
+      {
+        return col
+      }
+
+      col += 1
+    }
+
+    return col
+  }
+
+  func getContext(index: Index) -> (Range<Index>, Int, Int)
+  {
+    var index = index
+    var endIndex = index
+    var col = 1
+    var line = 1
+    var lastWasLF = false
+
+    while index != self.start
+    {
+      let newStartIndex = self.characters.index(before: index)
+      if self.characters[newStartIndex] == "\n"
+      {
+        lastWasLF = true
+        break
+      }
+      if self.characters[index] == "\r"
+      {
+        break
+      }
+
+      index = newStartIndex
+      col += 1
+    }
+
+    let startIndex = index
+    while index != self.start
+    {
+      index = self.characters.index(before: index)
+      if self.characters[index] == "\r" && !lastWasLF
+      {
+        line += 1
+      }
+      if self.characters[index] == "\n"
+      {
+        lastWasLF = true
+        line += 1
+      }
+      else
+      {
+        lastWasLF = false
+      }
+    }
+
+    while endIndex != self.end && self.characters[endIndex] != "\n" && self.characters[endIndex] != "\r"
+    {
+      endIndex = self.characters.index(after: endIndex)
+    }
+
+    return (startIndex..<endIndex, line, col)
+  }
+}
